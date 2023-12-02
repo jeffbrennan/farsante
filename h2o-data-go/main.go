bc88dccc10a9347e910e2e214fc83bdb28e01b40
jeffbrennan <jeffbrennan10@gmail.com>
2023-12-02 00:10:49 -0600

    add progress bar

--- h2o-data-go/main.go
+++ h2o-data-go/main.go
@@ -6,44 +6,39 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+
+	"github.com/schollz/progressbar/v3"
 )
+
 type GroupBy struct {
     id1 int
     id2 int
 }
-func main() {
-  
-	n_rows := 1000
 
-	val_id1 := rand.Perm(n_rows)
-	val_id2 := rand.Perm(n_rows)
-	
+func main() {
+  	const n_rows int = 100_000_000
 	records := []GroupBy{}
 	for i := 0; i < n_rows; i++ {
-		records = append(records, GroupBy{
-			id1: val_id1[i],
-			id2: val_id2[i],
-		})
-
-
+		records = append(records, GroupBy{rand.Intn(100), rand.Intn(100)})
+	}
+	
     file, err := os.Create("records.csv")
     if err != nil {
         log.Fatalln("failed to open file", err)
 		defer file.Close()
     }
-	
+
+	bar := progressbar.Default(int64(n_rows))
     w := csv.NewWriter(file)
 	err = w.Write([]string{"id1", "id2"})
 	if err != nil {
 		log.Fatalln("error writing record to file", err)
+		defer w.Flush()
 	}
 
-    defer w.Flush()
-    for _, record := range records {
-        row := []string{strconv.Itoa(record.id1), strconv.Itoa(record.id2)}
-        if err := w.Write(row); err != nil {
-            log.Fatalln("error writing record to file", err)
-        }
+	for _, record := range records {
+		bar.Add(1)
+		row := []string{strconv.Itoa(record.id1), strconv.Itoa(record.id2)}		
+		w.Write(row)
     }   
-}
 }
