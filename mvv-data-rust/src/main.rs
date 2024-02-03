43f16a1fbe5c2d84698817365d8390eca03e3a41
jeffbrennan <jeffbrennan10@gmail.com>
2024-02-03 01:55:49 -0500

    add progress bar

--- mvv-data-rust/src/main.rs
+++ mvv-data-rust/src/main.rs
@@ -1,28 +1,29 @@
 use polars::prelude::*;
 use std::{fs::File, ops::Range};
 
+use kdam::{tqdm, BarExt};
+
 fn main() -> Result<(), PolarsError> {
-    let n = 10_000_000_000;
-    let n_chunks = 128;
+    let n = 1_000_000_000;
+    let n_chunks = 16;
     let output_dir = "test";
+
     std::fs::create_dir_all(output_dir).unwrap();
+    let mut pb = tqdm!(total = n as usize);
 
     for chunk in 0..n_chunks {
-        println!("writing chunk: {}", chunk);
         let start = (n / n_chunks) * chunk;
         let end = (n / n_chunks) * (chunk + 1);
+        let n_rows: i64 = end - start;
         let index: Range<i64> = start..end;
         let mvv: Range<i64> = start..end;
-        let mut df =
+        let mut df: DataFrame =
             DataFrame::new(vec![Series::new("index", index), Series::new("mvv", mvv)]).unwrap();
 
         let mut file = File::create(format!("{}/example_{}.parquet", output_dir, chunk))
             .expect("could not create file");
-        let result = ParquetWriter::new(&mut file).finish(&mut df);
-        match result {
-            Ok(_) => println!("Success writing chunk: {}", chunk),
-            Err(e) => println!("Error: {}", e),
-        }
+        let _ = ParquetWriter::new(&mut file).finish(&mut df);
+        let _ = pb.update(n_rows as usize);
     }
     Ok(())
 }
